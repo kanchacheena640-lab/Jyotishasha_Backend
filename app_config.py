ecd225bd6ec81ae76d480c0d6a44e924ff0a5425
Suvi Vish <kanchacheena640@gmail.com>
2025-09-07 23:05:02 +0530

    Use env DATABASE_URL instead of localhost

--- app_config.py
+++ app_config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Ravi!123@localhost:5432/jyotishasha_db"
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-this-in-prod")
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
