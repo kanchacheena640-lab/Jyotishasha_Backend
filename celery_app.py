8c8d902fce464d7bc4bc9d0b49577eff3aa980b3
Suvi Vish <kanchacheena640@gmail.com>
2025-09-09 12:42:52 +0530

    Celery Change after updating Redis url

--- celery_app.py
+++ celery_app.py
@@ -5,11 +5,10 @@ from celery import Celery
 
 def make_celery():
     redis_url = os.getenv("REDIS_URL")
-    if redis_url.startswith("rediss://"):
-        # For Upstash SSL connection
-        broker_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
+    if not redis_url:
+        print("[ERROR] REDIS_URL is missing!")
     else:
-        broker_use_ssl = None
+        print("[INFO] Redis URL loaded:", redis_url)
 
     celery = Celery(
         "jyotishasha",
@@ -17,14 +16,11 @@ def make_celery():
         backend=redis_url,
         include=["tasks"]
     )
-
     celery.conf.update(
         task_serializer='json',
         result_serializer='json',
-        accept_content=['json'],
-        broker_use_ssl=broker_use_ssl  # ⬅️ Important for rediss://
+        accept_content=['json']
     )
-
     return celery
 
 celery = make_celery()
