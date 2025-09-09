4e23f66fcbd9cf30edf90fdde016260f26310e15
Suvi Vish <kanchacheena640@gmail.com>
2025-09-09 13:24:02 +0530

    final fix for celery

--- celery_app.py
+++ celery_app.py
@@ -7,7 +7,7 @@ redis_url = os.getenv("REDIS_URL")
 if not redis_url:
     raise ValueError("[ERROR] REDIS_URL is missing or not set in Render environment variables.")
 else:
-    print("[INFO] Redis URL loaded successfully.")
+    print("[INFO] Redis URL loaded successfully:", redis_url)
 
 celery = Celery(
     "jyotishasha",
@@ -16,8 +16,11 @@ celery = Celery(
     include=["tasks"]
 )
 
+# SSL config for rediss:// (secure Redis)
 celery.conf.update(
     task_serializer='json',
     result_serializer='json',
-    accept_content=['json']
+    accept_content=['json'],
+    broker_use_ssl={"ssl_cert_reqs": "CERT_NONE"},
+    redis_backend_use_ssl={"ssl_cert_reqs": "CERT_NONE"},
 )
