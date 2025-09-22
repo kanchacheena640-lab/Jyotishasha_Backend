a2e168be9760aeed134cc7fdf7f2f23faebaf5d4
Suvi Vish <kanchacheena640@gmail.com>
2025-09-22 18:09:04 +0530

    test

--- config/pricing.py
+++ config/pricing.py
@@ -24,6 +24,6 @@ PRODUCT_PRICES = {
     "property_report": 25,
     "saturn_transit_report": 25,
     "second_marriage_report": 25,
-    "divorce_possibility_report": 25,
+    "divorce_possibility_report": 1,
     # Add more here...
 }
