82d84657bf0f3922940991557cc5a0ec7da852a3
Suvi Vish <kanchacheena640@gmail.com>
2025-12-13 00:05:44 +0530

    extensions edited

--- extensions.py
+++ extensions.py
@@ -1,5 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
+import firebase_admin
+from firebase_admin import credentials
+import os
+import json
+
 db = SQLAlchemy()
 jwt = JWTManager()
+
+# -------------------------------------------------
+# 🔥 Firebase Admin Initialization (ONCE)
+# -------------------------------------------------
+if not firebase_admin._apps:
+    # Render me JSON string env me hota hai
+    firebase_json = os.environ.get("FCM_SERVICE_ACCOUNT_JSON")
+
+    if firebase_json:
+        cred = credentials.Certificate(json.loads(firebase_json))
+        firebase_admin.initialize_app(cred)
+    else:
+        raise RuntimeError("FCM_SERVICE_ACCOUNT_JSON env variable not set")
