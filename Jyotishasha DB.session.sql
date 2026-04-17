50d8d43995653df4f91e039ed2642baab147369e
Suvi Vish <kanchacheena640@gmail.com>
2026-04-17 13:30:33 +0530

    db entry

--- Jyotishasha DB.session.sql
+++ Jyotishasha DB.session.sql
@@ -1,1 +1,7 @@
-SELECT id, firebase_uid FROM app_users WHERE id = 278;
+INSERT INTO user_notifications (user_id, title, body, is_read)
+SELECT id,
+       '🎉 Congratulations!',
+       'Now you will receive Tithi & Transit related alerts directly on your Jyotishasha notifications 🔔',
+       false
+FROM app_users
+WHERE fcm_token IS NOT NULL;
